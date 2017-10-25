191f7ef5d4f2081d84b03dc65325c7b812f64d8b
Mohammed Barzinji <mohammedbarzinji@gmail.com>
2017-10-25 16:20:47 +0200

    last changes

--- app/design/frontend/Dckap/lumachild/registration.php
+++ app/design/frontend/Dckap/lumachild/registration.php
@@ -1,10 +1,6 @@
 <?php
-
 \Magento\Framework\Component\ComponentRegistrar::register(
-
    \Magento\Framework\Component\ComponentRegistrar::THEME,
-
    'frontend/Dckap/lumachild',
-
    __DIR__
 );
--- app/design/frontend/Magento/mytheme/registration.php
+++ app/design/frontend/Magento/mytheme/registration.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Copyright © 2016 Magento. All rights reserved.
+ * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'adminhtml/Lani/my-first-admin-theme', // Example: 'adminhtml/Magento/backend'
+    'frontend/Magento/mytheme',
     __DIR__
 );
