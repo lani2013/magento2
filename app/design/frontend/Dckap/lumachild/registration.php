88cf0960fbfd291510c9563ecd7a6b0f482de083
Mohammed <mohammed@dev001.office.sharis.de>
2017-10-25 13:55:03 +0200

    Dckap theme creation

---
+++ app/design/frontend/Dckap/lumachild/registration.php
@@ -0,0 +1,10 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+
+   \Magento\Framework\Component\ComponentRegistrar::THEME,
+
+   'frontend/Dckap/lumachild',
+
+   __DIR__
+);
